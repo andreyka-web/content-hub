77e844ddb4536aef09084adf69e11c11584524d3
Andrii Krot <andreyka.web@gmail.com>
2024-10-15 12:45:16 -0400

    api file cintroller

--- hub-api/app/Http/Controllers/FileController.php
+++ hub-api/app/Http/Controllers/FileController.php
@@ -6,7 +6,9 @@ use App\Http\Requests\StoreFileRequest;
 use App\Http\Requests\UpdateFileRequest;
 use App\Models\File;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Gate; 
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class FileController extends Controller
 {
@@ -15,7 +17,7 @@ class FileController extends Controller
      */
     public function index()
     {
-        $user = Auth::user(); 
+        $user = Auth::user();
         return $user->files->all();
     }
 
@@ -24,17 +26,17 @@ class FileController extends Controller
      */
     public function store(StoreFileRequest $request)
     {
-        if($request->validated()){
+        if ($request->validated()) {
             $path = $request->file('file')->store('uloads');
- 
+
             $data = $request->safe()->all();
-            
-            $user = Auth::user(); 
+
+            $user = Auth::user();
             // create a record in db
             $request->user()->files()->create([
                 'filename' => $data['file']->getClientoriginalName(),
                 'user_id' => $user->id,
-                'category_id' => $data['category'] ?? null,  
+                'category_id' => $data['category'] ?? null,
                 'path' => $path
             ]);
 
@@ -49,15 +51,60 @@ class FileController extends Controller
      */
     public function show(File $file)
     {
-        return 'show';
+        Gate::authorize('view', $file);
+
+        if (!$file) {
+            return response()->json([
+                'message' => 'File not found for the specified user'
+            ], 404);
+        }
+
+        // Check if the file exists in storage
+        if (!Storage::exists($file->path)) {
+            return response()->json([
+                'message' => 'File not found in storage'
+            ], 404);
+        }
+
+        // Return the file as a response for display or download
+        return response()->file(Storage::path($file->path), [
+            'Content-Type' => Storage::mimeType($file->path)
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
     public function update(UpdateFileRequest $request, File $file)
-    {
-        return 'update';
+    {  
+        if (!$request->hasAny(['filename','file','category'])) {
+            return response()->json(['message' => 'No update required']);
+        }
+
+        $validator = Validator::make($request->all(), $request->rules());
+
+        if($validator->fails()){
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        if ($request->hasFile('file')) { 
+            Storage::delete($file->path);
+            $file->path = $request->file('file')->store('uloads'); 
+        }
+
+        $data = $validator->validated(); 
+
+        $file->filename = $data['filename'] ?? $file->filename;
+        $file->category_id = $data['category'] ?? $file->category_id;
+        $file->save();
+
+        return response()->json([
+            'message' => 'File updated successfully',
+            'file' => $file
+        ]);
     }
 
     /**
@@ -65,6 +112,15 @@ class FileController extends Controller
      */
     public function destroy(File $file)
     {
-        return 'destroy';
+        $user = Auth::user();
+        Gate::authorize('delete', [$user, $file]);
+
+        $user->files->where('id', $file->id)->delete();
+
+        if (Storage::exists($file->path)) {
+            Storage::delete($file->path);
+        }
+
+        return response()->json(['message' => "File {$file->filename} deleted sucessfully"]);
     }
 }
--- hub-api/routes/api.php
+++ hub-api/routes/api.php
@@ -4,12 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\FileController;
 use App\Http\Controllers\ReadFolderController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function() {
     Route::post('/logout', [AuthController::class, 'logout']);
@@ -19,6 +14,5 @@ Route::middleware('auth:sanctum')->group(function() {
 });
 
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-//Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']); 
 
