c3e021c0d34cfed85a7571cc74f397aa87d58d9b
Andrii <andreyka.web@gmail.com>
2025-01-30 15:12:46 -0500

    hub-fe file functionality fix

--- hub-fe/src/components/forms/FileEditForm.vue
+++ hub-fe/src/components/forms/FileEditForm.vue
@@ -3,34 +3,52 @@ import Alert from '../ui/Alert.vue';
 import Label from '../ui/Label.vue';
 import Input from '../ui/Input.vue';
 import Button from '../ui/Button.vue';
-import fetchData from '@/lib/fetchApi'
+import { api_url } from '@/lib/fetchApi'
+import { ref, inject } from 'vue';
 
 const { activeFile } = defineProps(['activeFile']);
-const emits = defineEmits(['file-saved']); 
+const emits = defineEmits(['file-saved']);
+ 
+var message = ref(null);
+var name = ref(activeFile.filename);
+var category = ref(activeFile.category_id);
 
-var file = null; 
-var message = null;
-var name = activeFile.filename.split('.').slice(0, -1).join('.');
-const UpdateFile = (event) => { file = event.target.files[0] }
+const getHeaders = inject('getHeaders');
 
 async function saveFile() {
-    var data = new FormData()
-    if (file) {
-        const fileData = new File([file], file.name, { type: file.type });
-        data.append('file', fileData)
-    }
-    data.append('category', activeFile.category_id)
-    data.append('name', name)
+    const formData = new FormData() 
+    formData.append('name', name.value)
+    formData.append('category', category.name) 
 
-    fetchData(`file/${activeFile.id}`, data, "PUT", true)
-        .then(json => {
-            message = json.hasOwnProperty('errors') ? json.message : null;
-            if (message == null) {
-                emits('file-saved');
-            }
+    const jsonBody = JSON.stringify({
+                'name': name.value,
+                'category': category.value
+            });
+
+    try {
+        const headers = getHeaders();
+        headers['Content-Type'] = 'application/json';
+        headers['Content-Length'] = jsonBody.length.toString();
+        const response = await fetch(`${api_url}file/${activeFile.id}`, {
+            method: 'PUT',
+            body: jsonBody,
+            headers: headers
         });
+        if(!response.ok) {
+            const json = await response.json();
+            message.value = json.message;
+        }
+        else {
+            emits('file-saved');
+            message.value = null; 
+            name.value = null;
+            category.value = null;
+        }
+    } catch(error) {
+        console.error(error);
+        message.value = error.message;
+    }
 };
-
 </script>
 
 <template>
@@ -38,8 +56,6 @@ async function saveFile() {
         <Alert type="error" v-if="message">{{ message }}</Alert>
         <Label for="filename">Rename File</Label>
         <Input name="filename" id="filename" type="text" v-model="name" />
-        <Label for="file">Replace File <small class="font-thin ">(old file will be lost)</small></Label>
-        <Input name="file" id="file" type="file" @change="UpdateFile" />
         <Button>Save File</Button>
     </form>
 </template>
