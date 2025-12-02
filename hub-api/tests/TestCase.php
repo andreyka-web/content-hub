ba1f820b5ad0aaa74982cedb669f91ac85358cae
Andrii <andreyka.web@gmail.com>
2025-12-01 22:36:14 -0500

    security patches

--- hub-api/tests/TestCase.php
+++ hub-api/tests/TestCase.php
@@ -3,8 +3,17 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\Models\User;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected $user;
+
+    protected function setUp(): void 
+    {
+        parent::setUp();
+  
+        $this->user = User::factory()->create();
+    }
+
 }
